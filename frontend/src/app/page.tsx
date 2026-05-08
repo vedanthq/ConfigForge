b0b8ab647e1f404e6ef01fcd4eab93e8294c34e4
Vedant Singh <teleported0722@gmail.com>
2026-05-08 17:01:32 +0530

    feat(frontend): add home page with AppShell and GeneratorUI integration

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import AppShell from "@/components/pages/AppShell";
+import GeneratorUI from "@/components/GeneratorUI";
+
+export default function Home() {
+  return (
+    <>
+      <AppShell />
+      <GeneratorUI />
+    </>
+  );
+}
